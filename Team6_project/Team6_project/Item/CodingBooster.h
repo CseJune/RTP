3df68ea476edcf627695b7489eab02c66446ead0
Hyun <beevvv12@gmail.com>
2025-01-11 16:43:05 +0900

    feat: 아이템 세부구현
    -codingBooster 세부구현

--- Team6_project/Team6_project/Item/CodingBooster.h
+++ Team6_project/Team6_project/Item/CodingBooster.h
@@ -7,10 +7,10 @@ using namespace std;
 class CodingBooster
 {
 public:
-    string name;
-    int CodingIncrease;
-
-    CodingBooster();
-    string getName();
-    void use(Character* character);
+    string name;    // 아이템 이름   
+    int codingIncrease; // 코딩력 증가량
+    
+    CodingBooster();    // 생성자
+    string GetName();   // 아이템 이름 반환
+    void Use(Character* character); // 캐릭터 코딩력 증가 효과 적용
 };
