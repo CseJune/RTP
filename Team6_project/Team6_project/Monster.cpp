c4a129a470bf8bf0674b63640deacd18b7a80e50
ishyun1227 <ishyun1227@naver.com>
2025-01-11 23:41:55 +0900

    feat Monster
    튜터님들 클래스 삭제, 클래스 생성자 수정

--- Team6_project/Team6_project/Monster.cpp
+++ Team6_project/Team6_project/Monster.cpp
@@ -8,12 +8,6 @@
 
 using namespace std;
 
-//string getName();
-//int getHp();
-//int getAdd();
-//void takeDamage(int
-//	Item* dropItem();
-
 // Tutor 매서드
 
 string Tutor::getName()
@@ -38,12 +32,57 @@ void Tutor::takeDamage(int add)
 
 // ManagerTutor 매서드
 
-ManagerTutor::ManagerTutor(string name, int hp, int add)
-{	
-	name = "";
-	randome_device rd;
+ManagerTutor::ManagerTutor(Character a, string name) : Tutor(a, name)
+{
+	random_device rd;
 	mt19937 gen(rd());
-	uniform_int_distribution<int> dis(Character a.getLevel());
+	uniform_int_distribution<int> dis(a.getLevel() * 50, a.getLevel() * 55);
+	setHp(dis(gen));
 
+	uniform_int_distribution<int> dis(a.getLevel() * 5, a.getLevel() * 10);
+	setAdd(dis(gen));
+}
 
+BasicTutor::BasicTutor(Character a, string name) : Tutor(a, name)
+{
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<int> dis(a.getLevel() * 70, a.getLevel() * 75);
+	setHp(dis(gen));
+
+	uniform_int_distribution<int> dis(a.getLevel() * 10, a.getLevel() * 15);
+	setAdd(dis(gen));
+}
+
+StandardTutor::StandardTutor(Character a, string name) : Tutor(a, name)
+{
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<int> dis(a.getLevel() * 100, a.getLevel() * 110);
+	setHp(dis(gen));
+
+	uniform_int_distribution<int> dis(a.getLevel() * 20, a.getLevel() * 25);
+	setAdd(dis(gen));
+}
+
+ChallengeTutor::ChallengeTutor(Character a, string name) : Tutor(a, name)
+{
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<int> dis(a.getLevel() * 150, a.getLevel() * 160);
+	setHp(dis(gen));
+
+	uniform_int_distribution<int> dis(a.getLevel() * 30, a.getLevel() * 35);
+	setAdd(dis(gen));
+}
+
+BossTutor::BossTutor(Character a, string name) : Tutor(a, name)
+{
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<int> dis(a.getLevel() * 200, a.getLevel() * 210);
+	setHp(dis(gen));
+
+	uniform_int_distribution<int> dis(a.getLevel() * 50, a.getLevel() * 60);
+	setAdd(dis(gen));
 }
