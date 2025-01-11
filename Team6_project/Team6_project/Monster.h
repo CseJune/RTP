7c25e01f4e12caaef353c3559b1d1b37ddb53187
ishyun1227 <ishyun1227@naver.com>
2025-01-11 16:51:35 +0900

    feat Monster
    1. Monster.h 안의 클래스들 선언 중
    2. Monmster.cpp 안의 클래스 구현 중

--- Team6_project/Team6_project/Monster.h
+++ Team6_project/Team6_project/Monster.h
@@ -1,20 +1,102 @@
 #pragma once
-#include"Character.h"
-#include"Monster.h"
+#include "Character.h"
+#include "Item.h"
 #include <iostream>
-#include <vector>
 #include <string>
 
 using namespace std;
 
-class Monster
+class Tutor
 {
 public:
-	Monster();
+	Tutor(string name, int hp, int add) : name(name), hp(hp), add(add) {}
 
-	virtual string getname();
-	virtual int gethp();
-	virtual int getadd();
-	virtual void takedamage(int damage);
+	virtual string getName();
+	virtual int getHp();
+	virtual int getAdd();
+	virtual void takeDamage(int add);
+	virtual Item* dropItem();
+
+protected:
+	string name;
+	int hp;
+	int add;
+};
+
+class ManagerTutor : Tutor 
+{
+public:
+	ManagerTutor(string name, int hp, int add) : Tutor(name, hp, add) {}
+	string getName() override;
+	int getHp() override;
+	int getAdd() override;
+	void takeDamage(int add) override;
+	Item* dropItem() override;
+};
+
+class BaeManager : public ManagerTutor 
+{
+	BaeManager(string name, int hp, int add) : ManagerTutor(name = "πË¿Á»Ò ∏≈¥œ¿˙¥‘", hp, add) {}
+};
+
+class ParkManager : public ManagerTutor
+{
 
 };
+
+class BasicTutor : public Tutor
+{
+	BasicTutor(string name, int hp, int add) : Tutor(name, hp, add) {}
+};
+
+class HanBasic : public BasicTutor
+{
+
+};
+
+class ParkJungBasic : public BasicTutor
+{
+
+};
+
+class ParkKyungBasic : public BasicTutor
+{
+
+};
+
+class StandardTutor : public Tutor
+{
+	StandardTutor(string name, int hp, int add) : Tutor(name, hp, add) {}
+};
+
+class MoonStandard : public StandardTutor
+{
+
+};
+
+class ChoiStandard : public StandardTutor
+{
+
+};
+
+class ChallengeTutor : public Tutor
+{
+	ChallengeTutor(string name, int hp, int add) : Tutor(name, hp, add) {}
+};
+
+class KangChallenge : public ChallengeTutor
+{
+
+};
+
+class BossTutor : public Tutor
+{
+	BossTutor(string name, int hp, int add) : Tutor(name, hp, add) {}
+};
+
+class KimBoss : public BossTutor
+{
+
+};
+
+
