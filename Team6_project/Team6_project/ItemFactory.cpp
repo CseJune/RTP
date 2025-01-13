2dec4a45afef0a5c233c7bf7e2362424612cbf67
Hyun <beevvv12@gmail.com>
2025-01-13 16:41:45 +0900

    refactor: ItemFactory 리팩토링
    -매핑을 이용해 성능, 확장성, 유지보수성 개선

--- Team6_project/Team6_project/ItemFactory.cpp
+++ Team6_project/Team6_project/ItemFactory.cpp
@@ -1,32 +1,38 @@
+#include "ItemFactory.h"
+
+#include <functional>
+#include <unordered_map>
+#include <string>
+
 #include "ChatGPT.h"
-#include "ItemFactory.h" 
 #include "HealthPotion.h" 
 #include "CodingBooster.h"
 #include "MaxHealthPotion.h" 
 #include "TutorAttackReduction.h"
+#include "TutorHealthReduction.h"
 
-// CreateItem 구현
-Item* ItemFactory::CreateItem(const string& itemType)
+using namespace std;
+
+// 아이템 타입과 생성 함수 매핑 초기화
+unordered_map<string, function<Item*()>> ItemFactory::itemRegistry = {
+    {"ChatGPT",[]() { return new ChatGPT(); }},
+    {"HealthPotion", []() { return new HealthPotion(); }},
+    {"MaxHealthPotion", []() { return new MaxHealthPotion(); }},
+    {"CodingBooster", []() { return new CodingBooster(); }},
+    {"TutorAttackReduction", []() {return new TutorAttackReduction(); }},
+    {"TutorHealthReduction",[]() {return new TutorHealthReduction(); }}
+    };
+
+// 아이템 생성 메서드
+Item* ItemFactory::createItem(const string& itemType)
 {
-    if (itemType == "HealthPotion")
-    {
-        return new HealthPotion();
-    }
-    else if (itemType == "CodingBooster")
-    {
-        return new CodingBooster();
-    }
-    else if (itemType == "MaxHealthPotion")
-    {
-        return new MaxHealthPotion();
-    }
-    else if (itemType == "TutorAttackReduction")
-    {
-        return new TutorAttackReduction();
-    }
-    else if (itemType == "ChatGPT")
-    {
-        return new ChatGPT();
-    }
-    return nullptr; // 일치하는 타입이 없으면 nullptr 반환
+    auto it = itemRegistry.find(itemType);
+    // 해당 아이템이 있으면 생성 함수 호출, 없으면 nullptr 반환
+    return (it != itemRegistry.end()) ? it->second() : nullptr;
+}
+
+// 아이템 등록 메서드
+void ItemFactory::registerItem(const string& itemType, function<Item*()> itemCreator)
+{
+    itemRegistry[itemType] =itemCreator;
 }
--- Team6_project/Team6_project/ItemFactory.h
+++ Team6_project/Team6_project/ItemFactory.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
-
+#include <functional>
+#include <unordered_map>
 #include <string>        
 #include "Item.h"     
 
@@ -9,6 +10,13 @@ using namespace std;
 class ItemFactory
 {
 public:
-    // 정적 메서드: 아이템 타입에 따라 객체 생성
-    static Item* CreateItem(const string& itemType);
+    // 아이템 타입과 생성 함수를 매핑하는 맵
+    static unordered_map<string, function<Item*()>> itemRegistry;
+
+private:
+    // 아이템 생성 메서드
+    static Item* createItem(const string& itemType);
+    
+    // 아이템 등록 메서드
+    static void registerItem(const string& itemType, function<Item*()> createItem);
 };
