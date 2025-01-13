8e706b3c0e814177a51c035d4f5c1d140241f827
CseJune <tpwns9541@gmail.com>
2025-01-13 16:50:32 +0900

    fix, feat, add inventory and character
    1. 인벤토리 기능과 캐릭터 기능을 분리했습니다
    
    2. 분리한 방법은 기존 인벤토리 클래스가 캐릭터 헤더 파일 안에 있었지만 인벤토리 헤더 파일을 새로 생성했습니다. 마찬가지로 인벤토리 cpp파일도 생성했습니다.
    
    3. 인벤토리 기능을 동적으로 변환했습니다.

---
+++ Team6_project/Team6_project/Inventory.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Item.h" // 아이템 관련 클래스 필요시 포함
+
+class Character; // 전방선언
+
+// Inventory 클래스 선언
+class Inventory
+{
+public:
+    void addItem(Item* item); // 아이템 추가
+    void displayInventory() const;
+    bool useItem(const string& itemName, Character* character);
+
+private:
+    vector<Item*> items;
+};
